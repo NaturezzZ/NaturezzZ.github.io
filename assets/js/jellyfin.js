3526db0ff87a33649d47e3b738d0535f9bc921d8
NaturezzZ <naturezzz@outlook.com>
2024-01-05 16:46:48 +0800

    nits

--- assets/js/jellyfin.js
+++ assets/js/jellyfin.js
@@ -1,28 +1,36 @@
 window.onload = function () {
+    const urlA_test = "http://lab.zhengnq.com:8096/web/bc8d51405ec040305a87.ico"
     const urlA = "http://lab.zhengnq.com:8096/web/index.html";
     const urlB = "http://39.99.250.62:8096/web/index.html";
 
     const timeout = 1000; // 超时时间设置为 1 秒（1000 毫秒）
 
-    // 创建一个超时承诺
-    const timeoutPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject(new Error('Request timed out'));
-        }, timeout);
-    });
+    let imageLoaded = false;
 
-    // 使用 Promise.race 竞赛
-    Promise.race([fetch(urlA, { mode: 'no-cors', referrerPolicy: 'no-referrer' }), timeoutPromise])
-        .then(response => {
-            if (response.ok || response.type === 'opaque') {
-                window.location.href = urlA;
-            } else {
-                window.location.href = urlB;
-            }
-        })
-        .catch(error => {
-            console.log('Error or timeout:', error);
-            window.location.href = urlB;
-        });
+    const testImage = new Image();
+
+    testImage.onload = function() {
+        if (!imageLoaded) {
+            imageLoaded = true;
+            window.location.href = urlA; // 如果图片加载成功，跳转到网站A
+        }
+    };
+
+    testImage.onerror = function() {
+        if (!imageLoaded) {
+            imageLoaded = true;
+            window.location.href = urlB; // 如果图片加载失败，跳转到网站B
+        }
+    };
+
+    testImage.src = urlA_test; // 尝试加载图片
+
+    // 设置超时检测
+    setTimeout(function() {
+        if (!imageLoaded) {
+            imageLoaded = true;
+            window.location.href = urlB; // 如果超时，跳转到网站B
+        }
+    }, timeout);
 }
 
